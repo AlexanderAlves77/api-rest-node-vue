eec100a6c4b5dcd4524ae20d0fa521be037c713e
Alexander Alves <vendas77.rj@gmail.com>
2022-02-16 14:38:56 -0300

    Adicionando o middleware

---
+++ backend/middleware/AdminAuth.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken')
+const secret = "AF52NAKN654GQWA77JTYLJ3214MRV"
+
+module.exports = function(req, res, next) {
+
+    const authToken = req.headers['authorization']
+
+    if (authToken !== undefined) {
+        const bearer = authToken.split(' ')
+        let token = bearer[1]
+
+        try {
+            const decoded = jwt.verify(token, secret)
+            
+            if (decoded.roles === 1) {
+                next()
+            } else {
+                res.status(403)
+                res.send("Você não têm permissão para acessar!")
+                return
+            }
+            
+        } catch (error) {
+            res.status(403)
+            res.send("Você não está autenticado")
+            return
+        }    
+        
+    } else {
+        res.status(403)
+        res.send("Você não está autenticado")
+        return
+    }
+}
